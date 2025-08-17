5ee1db812b50444d4e3cd30cb266d69de82bc5ca
Emily Zakas <emily.zakas@gmail.com>
2025-08-16 22:52:02 -0400

    Initial Commit

---
+++ Tetrahedron2/Tetrahedron2App.swift
@@ -0,0 +1,17 @@
+//
+//  Tetrahedron2App.swift
+//  Tetrahedron2
+//
+//  Created by Emily Zakas on 8/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Tetrahedron2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tetrahedron2Tests/Tetrahedron2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Tetrahedron2Tests.swift
+//  Tetrahedron2Tests
+//
+//  Created by Emily Zakas on 8/16/25.
+//
+
+import Testing
+@testable import Tetrahedron2
+
+struct Tetrahedron2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
